1db9bf916482c662d6f4b04229470ad6dd147abd
mileine <mileine.assato@gmail.com>
2023-04-21 18:30:41 -0300

    update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,17 @@
-import { useEffect, useContext, useState } from 'react'
+import { useContext } from 'react'
 import './styles/App.scss'
-import { formatCountryData } from './utils/utils'
-import { CountryType } from './context/AppContext'
-import { api } from './services/api'
-import countryData from './data/mock.json'
-import CountryCard from './components/CountryCard/CountryCard'
-import CountriesContainer from './components/CountriesContainer/CountriesContainer'
 import Header from './components/Header/Header'
 import AppContext, { AppContextType } from './context/AppContext'
+import Home from './pages/Home/Home'
 
 function App() {
-  const initialState: CountryType = formatCountryData(countryData[0])
   const { appState }: AppContextType = useContext(AppContext)
-  const countriesInitialState: CountryType[] | any[] = []
-  const regionOptions: string[] = ["Africa","America","Asia","Europe","Oceania"] 
-  // [HELP!] Como faço essa linha funcionar sem ter um initialState? 👇
-  const [country, setCountry] = useState(() => initialState)
-  const [countries, setCountries] = useState(()=> countriesInitialState)
-  const [searchTerm, setSearchTerm] = useState("")
-  const [selectedRegion, setSelectedRegion] = useState("")
 
-  useEffect(() => {
-    // [HELP!] Como posso organizar jeito melhor essas chamadas fora deste arquivo?
-    api
-      .get(`/name/brazil`)
-      .then(response => setCountry(formatCountryData(response.data[0])))
-      .catch(err => {
-        console.error("ops! ocorreu um erro " + err)
-      })
-  },[])
-
-
-  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if(event.key === 'Enter') {
-      api
-      .get(`/name/${searchTerm}`)
-      .then(response => setCountry(formatCountryData(response.data[0])))
-      .catch(err => {
-        console.error("ops! ocorreu um erro " + err)
-      })
-    }
-  }
-
-  useEffect(() => {
-    api
-      .get(`/alpha?codes=DE,US,BR,IS,AF,AX,AL,DZ`)
-      .then(response => {
-        const updatedList: CountryType[] = []
-        response.data.map((item: CountryType) => {
-          updatedList.push(formatCountryData(item))
-        })
-        setCountries(updatedList)
-        console.log(response.data)
-      })
-      .catch(err => {
-        console.error("ops! ocorreu um erro " + err)
-      })
-
-  },[searchTerm])
-
-  useEffect(()=>{
-    if(selectedRegion !== "") {
-      console.log(selectedRegion)
-      api
-      .get(`region/${selectedRegion}`)
-      .then(response => {
-        const updatedList: CountryType[] = []
-        if(selectedRegion === "Asia") console.log(response.data)
-        response.data.map((item: CountryType) => {
-          updatedList.push(formatCountryData(item))
-        })
-        setCountries(updatedList)
-        console.log(response.data)
-      })
-      .catch(err => {
-        console.error("ops! ocorreu um erro " + err)
-      })
-    }
-  },[selectedRegion])
 
   return (
     <div className={`app ${appState.themeLight ? 'theme--light': 'theme--dark'}`}>
       <Header />
-      <div className="content">
-        <input name="search" type="text" placeholder="search for a country..." value={searchTerm} onChange={(e)=>setSearchTerm(e.currentTarget.value)} onKeyDown={handleSearch}/>
-        {/* <div className="countries-container"> */}
-          <h3>search result</h3>
-          <CountryCard country={country}/>
-        {/* </div>      */}
-        <div className="filter-options">
-        <h3>view other countries</h3>
-         {/* TODO: Turn select into component  */}
-         <select name="" onChange={(e) => setSelectedRegion(e.currentTarget.value) }>
-            <option>Filter by Region</option>
-            {
-              regionOptions.map((item, index) => (
-                <>
-                  <option key={index} value={item}>{item}</option>
-                </>
-              ))
-            }
-          </select>
-        </div>
-        { countries && 
-          <CountriesContainer countries={countries} />
-        }
-      </div>
+      <Home />
       <div className="footer">
         <span>This is a <a href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca">Frontend Mentor</a> challenge | WIP with lots of ❤️ | <a href="https://github.com/mileine/frontendmentor-rest-countries-api">View on Github</a>  </span>
       </div>
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,108 @@
+import { useContext, FC, useState, useEffect } from 'react';
+import AppContext, { AppContextType, CountryType } from '../../context/AppContext'
+import './home.scss'
+import { formatCountryData } from '../../utils/utils';
+import { api } from '../../services/api';
+import CountryCard from '../../components/CountryCard/CountryCard';
+import countryData from '../../data/mock.json'
+import CountriesContainer from '../../components/CountriesContainer/CountriesContainer';
+
+const Home: FC = () => {
+  const initialState: CountryType = formatCountryData(countryData[0])
+  const { appState }: AppContextType = useContext(AppContext)
+  const countriesInitialState: CountryType[] | any[] = []
+  const regionOptions: string[] = ["Africa","America","Asia","Europe","Oceania"] 
+  // [HELP!] Como faço essa linha funcionar sem ter um initialState? 👇
+  const [country, setCountry] = useState(() => initialState)
+  const [countries, setCountries] = useState(()=> countriesInitialState)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [selectedRegion, setSelectedRegion] = useState("")
+
+  useEffect(() => {
+    // [HELP!] Como posso organizar jeito melhor essas chamadas fora deste arquivo?
+    api
+      .get(`/name/brazil`)
+      .then(response => setCountry(formatCountryData(response.data[0])))
+      .catch(err => {
+        console.error("ops! ocorreu um erro " + err)
+      })
+  },[])
+
+
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === 'Enter') {
+      api
+      .get(`/name/${searchTerm}`)
+      .then(response => setCountry(formatCountryData(response.data[0])))
+      .catch(err => {
+        console.error("ops! ocorreu um erro " + err)
+      })
+    }
+  }
+
+  useEffect(() => {
+    api
+      .get(`/alpha?codes=DE,US,BR,IS,AF,AX,AL,DZ`)
+      .then(response => {
+        const updatedList: CountryType[] = []
+        response.data.map((item: CountryType) => {
+          updatedList.push(formatCountryData(item))
+        })
+        setCountries(updatedList)
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.error("ops! ocorreu um erro " + err)
+      })
+
+  },[searchTerm])
+
+  useEffect(()=>{
+    if(selectedRegion !== "") {
+      console.log(selectedRegion)
+      api
+      .get(`region/${selectedRegion}`)
+      .then(response => {
+        const updatedList: CountryType[] = []
+        if(selectedRegion === "Asia") console.log(response.data)
+        response.data.map((item: CountryType) => {
+          updatedList.push(formatCountryData(item))
+        })
+        setCountries(updatedList)
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.error("ops! ocorreu um erro " + err)
+      })
+    }
+  },[selectedRegion])
+
+  return (
+    <div className="home">
+      <input name="search" type="text" placeholder="search for a country..." value={searchTerm} onChange={(e)=>setSearchTerm(e.currentTarget.value)} onKeyDown={handleSearch}/>
+      {/* <div className="countries-container"> */}
+        <h3>search result</h3>
+        <CountryCard country={country}/>
+      {/* </div>      */}
+      <div className="filter-options">
+      <h3>view other countries</h3>
+        {/* TODO: Turn select into component  */}
+        <select name="" onChange={(e) => setSelectedRegion(e.currentTarget.value) }>
+          <option>Filter by Region</option>
+          {
+            regionOptions.map((item, index) => (
+              <>
+                <option key={index} value={item}>{item}</option>
+              </>
+            ))
+          }
+        </select>
+      </div>
+      { countries && 
+        <CountriesContainer countries={countries} />
+      }
+    </div>
+  );
+};
+
+export default Home;
