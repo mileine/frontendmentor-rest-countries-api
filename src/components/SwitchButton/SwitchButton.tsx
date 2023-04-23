62884fd3c15ad7e3637a12f203a98e5e232fc0a7
mileine <mileine.assato@gmail.com>
2023-04-23 12:38:32 -0300

    add icon to switch button

--- src/components/SwitchButton/SwitchButton.tsx
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -1,14 +1,23 @@
-import { useContext, FC } from 'react';
+import { useContext, FC, useEffect, useState } from 'react';
 import AppContext, { AppContextType } from '../../context/AppContext'
+import { ReactComponent as DarkIcon } from '../../assets/icon-dark.svg'
+import { ReactComponent as LightIcon} from '../../assets/icon-light.svg'
+
 import './switchButton.scss'
 
 const SwitchButton: FC = () => {
   const { appState, toggleTheme }: AppContextType = useContext(AppContext)
+  const [themeLight, setThemeLight] = useState(true)
+
+  useEffect(() => {
+    setThemeLight(appState.themeLight)
+  }, [appState.themeLight])
 
   return (
     <button className={`switch-button ${appState.themeLight? 'light' : 'dark'}`} onClick={() => toggleTheme()}>
-      {/* [DUNNO] How do I use images from folder assets? */}
-      {/* <img className="switch-button__icon" src={appState.themeLight? '/assets/icon-light.svg': '/assets/icon-dark.svg'} alt=""/> */}
+      {
+        themeLight ? <LightIcon /> : <DarkIcon />
+      }
       <span>Dark Theme</span>
     </button>
   );
