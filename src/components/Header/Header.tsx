22ca838589d227468192ed7189c84d92aeece03c
mileine <mileine.assato@gmail.com>
2025-11-09 13:56:55 -0300

    feat: make header title clickable to redirect to home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useContext, FC } from 'react';
+import { Link } from 'react-router-dom';
 import AppContext, { AppContextType } from '../../context/AppContext'
 import SwitchButton from '../SwitchButton/SwitchButton';
 
@@ -13,14 +14,16 @@ const Header: FC = () => {
       }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex justify-between items-center">
-        <h1 
-          className="text-sm sm:text-base md:text-xl lg:text-2xl font-extrabold"
-          style={{ 
-            color: appState.themeLight ? 'hsl(200, 15%, 8%)' : 'hsl(0, 0%, 100%)'
-          }}
-        >
-          Where in the world?
-        </h1>
+        <Link to="/">
+          <h1 
+            className="text-sm sm:text-base md:text-xl lg:text-2xl font-extrabold cursor-pointer hover:opacity-80 transition-opacity"
+            style={{ 
+              color: appState.themeLight ? 'hsl(200, 15%, 8%)' : 'hsl(0, 0%, 100%)'
+            }}
+          >
+            Where in the world?
+          </h1>
+        </Link>
         <SwitchButton />
       </div>
     </header>
